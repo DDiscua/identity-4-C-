77532fd1529f236478326392f782994c52ceace8
ddiscua <ddiscua.dragon@gmail.com>
2020-11-03 21:38:07 -0600

    uploading core

---
+++ IdentityProvider/Config.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using IdentityModel;
+using IdentityServer4;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+namespace IdentityProvider
+{
+    internal class Clients
+    {
+        public static IEnumerable<Client> Get()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "oauthClient",
+                    ClientName = "Test Client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets = new List<Secret> {new Secret("SuperSecretPassword".Sha256())}, 
+                    AllowedScopes = new List<string> {"api1.read", "api1.write"}
+                },
+                new Client
+                {
+                    ClientId = "oidcClient",
+                    ClientName = "Test Client APP",
+                    ClientSecrets = new List<Secret> {new Secret("SuperSecretPassword".Sha256())}, 
+                    
+                    AllowedGrantTypes = GrantTypes.Code,
+                    RedirectUris = new List<string> {"https://localhost:5002/signin-oidc"},
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
+                        "role",
+                        "api1.read"
+                    },
+
+                    RequirePkce = true,
+                    AllowPlainTextPkce = false
+                },
+                  new Client
+                {
+                    ClientId = "reactTest",
+                    ClientName = "Test Client APP",
+                    ClientSecrets = new List<Secret> {new Secret("SuperSecretPassword".Sha256())},
+
+                    AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        IdentityServerConstants.StandardScopes.Email,
+                        "role",
+                        "api1.read"
+                    },
+
+                    RequirePkce = true,
+                    AllowPlainTextPkce = false
+                }
+            };
+        }
+    }
+
+    internal class Resources
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email(),
+                new IdentityResource
+                {
+                    Name = "role",
+                    UserClaims = new List<string> {"role"}
+                }
+            };
+        }
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new[]
+            {
+                new ApiResource
+                {
+                    Name = "api1",
+                    DisplayName = "API #1",
+                    Description = "Allow the application to access API #1 on your behalf",
+                    Scopes = new List<string> {"api1.read", "api1.write"},
+                    ApiSecrets = new List<Secret> {new Secret("ScopeSecret".Sha256())}, // change me!
+                    UserClaims = new List<string> {"role"}
+                }
+            };
+        }
+
+        public static IEnumerable<ApiScope> GetApiScopes()
+        {
+            return new[]
+            {
+                new ApiScope("api1.read", "Read Access to API #1"),
+                new ApiScope("api1.write", "Write Access to API #1")
+            };
+        }
+    }
+
+    internal class Users
+    {
+        public static List<TestUser> Get()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "5BE86359-073C-434B-AD2D-A3932222DABE",
+                    Username = "scott",
+                    Password = "password",
+                    Claims = new List<Claim>
+                    {
+                        new Claim(JwtClaimTypes.Email, "scott@scottbrady91.com"),
+                        new Claim(JwtClaimTypes.Role, "admin")
+                    }
+                }
+            };
+        }
+    }
+}
